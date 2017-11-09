3e74430097ed356ae777f2692d8f028eb76ad7a9
Rajanikant Devmore <rajanikant@prodevans.com>
2017-11-09 12:13:27 +0530

    Routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    ProductComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path : 'members',
+        component : MembersComponent
+      },
+      {
+        path: 'product',
+        component: ProductComponent
+      },
+      {
+        path:'', // for the default component route
+        component: MyComponentComponent,
+        pathMatch: 'full'
+      }
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
